3f8642ae60064a1361edf5bb9203222c00fa30b9
Hake Pratiksha <86944858+pratikshahake26@users.noreply.github.com>
2023-05-31 19:36:10 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// AOS.init();
+
+const li=document.querySelectorAll(".nav-item");
+const sec=document.querySelectorAll("section");
+
+function activeMenu(){
+    let len=sec.length;
+    while(--len && window.scrollY + 97 < sec[len].offsetTop){}
+    li.forEach(ltx => ltx.classList.remove("active"));
+    li[len].classList.add("active");
+}
+activeMenu(); 
+window.addEventListener("scroll", activeMenu);
